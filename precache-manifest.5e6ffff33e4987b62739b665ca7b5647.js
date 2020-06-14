848d7ab9e80a83c805ec634e18798923e282706d
Oskar Gierszewski <gierszon10@gmail.com>
2020-06-14 22:21:01 +0200

    Updates

---
+++ precache-manifest.5e6ffff33e4987b62739b665ca7b5647.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689b251023c3e090b156e2c21da1e724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51abc985fa5ea4cd454a",
+    "url": "/static/css/main.8ba810f3.chunk.css"
+  },
+  {
+    "revision": "b0af2dd78ebd8c4cec6e",
+    "url": "/static/js/2.2f44bcbf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2f44bcbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51abc985fa5ea4cd454a",
+    "url": "/static/js/main.295804f0.chunk.js"
+  },
+  {
+    "revision": "97ad9e083897a8286841",
+    "url": "/static/js/runtime-main.d7d8550f.js"
+  }
+]);
--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 5001
-const hostname = 'localhost';
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/test', (req, res) => res.send('Hello test!'))
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
